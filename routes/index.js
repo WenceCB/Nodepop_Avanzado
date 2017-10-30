6af7fa3b97522b618cba000a8e1860130af28499
Wence Criado <wence74@gmail.com>
2017-10-30 20:52:59 +0100

    Recibo peticiones para cambiar el idioma

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const i18n = require('i18n');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+// Recibimos la petición de cambio de idioma
+
+router.get('/lang/:locale', (req, res, next) => {
+  const locale = req.params.locale;
+  const referer = req.query.redir || req.get('referer');
+  res.cookie('nodeapi-lang', locale, { maxAge: 900000, httpOnly: true });
+  res.redirect(referer);
 });
 
 module.exports = router;
